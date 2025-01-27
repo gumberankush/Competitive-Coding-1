a5a91004f251a2cdf7dc89601291a17587133034
Ankush Gumber <gumber@adobe.com>
2025-01-26 23:58:17 -0800

    Find Min element

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,23 @@
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
 
+// Given a list of n-1 integers and these integers are in the range of 1 to n.
+// There are no duplicates in list. One of the integers is missing in the list.
+// Write an efficient code to find the missing integer.
+class Problem1 {
+    public int findMin(int[] nums)  {
+        if(nums == null || nums.length == 0) return -1;
+        int low = 0, high = nums.length-1;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] - mid == 1){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return 1+low;
+    }
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,30 @@
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
+// Given an integer array nums which is sorted in ascending order and all of its elements are unique and given also an integer k,
+// return the kth missing number starting from the leftmost number of the array.
 
+class Solution {
+    public int missingElement(int[] nums, int k) {
+        if(nums == null || nums.length == 0 || k == 0) {
+            return -1;
+        }
+
+        int low = 0, high = nums.length-1;
+        int start = nums[low]-1;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+
+            int missingCount = nums[mid]-mid-nums[0];
+
+            if(missingCount < k) {
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+
+        // loop is exited when low > high. so either take high or low-1
+        return nums[0] + k + high;
+    }
+}
